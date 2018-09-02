5620338dc557e2734421f579cb33409e586ac853
Sarah <32165829+SarahArantes@users.noreply.github.com>
2018-09-02 19:32:31 -0300

    Add files via upload

---
+++ P0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* P0.c */
+/* Sarah Luiza Martins Arantes de Araujo */
+/* 11611EBI012 */
+
+int main(int argc, char ** argv)
+{
+int i;
+ if(argc > 1)
+    {
+        if(argc > 2)
+          for(i = 0; i < argv [2][0]- '0'la; i++)
+         printf("Valeu, %s\n", argv[1]);
+ else
+    printf("Saudacoes, %s\n", argv[1]);
+    }
+ else
+    printf("Ola!\n");
+
+    return 0;
+}

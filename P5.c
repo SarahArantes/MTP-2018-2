bfb72489e5f20315e3276c61f09935b2faffddec
Sarah <32165829+SarahArantes@users.noreply.github.com>
2018-12-11 21:28:39 -0200

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,1 +1,32 @@
+//Sarah Luiza Martins Arantes de Araujo
+//11611EBI012
 
+#include <stdio.h>
+
+typedef
+
+	unsigned long long int
+
+Bytes8;
+
+typedef
+
+	struct LCG { Bytes8 a, c, m, rand_max, atual; }
+
+LCG;
+
+
+
+void semente(LCG * r, Bytes8 seed) {
+
+	r->a = 0x5DEECE66DULL;
+
+	r->c = 11ULL;
+
+	r->m = (1ULL << 48);
+
+	r->rand_max = r->m - 1;
+
+	r->atual = seed;
+
+}
